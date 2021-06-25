6fa468dcef764dcfa12540a66d1a15868075288f
quobod <83623984+quobod@users.noreply.github.com>
2021-06-25 07:23:13 -0400

    Update input_validation.py

--- custom_modules/input_validation.py
+++ custom_modules/input_validation.py
@@ -1,22 +1,28 @@
 import re
 
-pLetters = "^[a-zA-Z]+$"
-pNumbers = "^[0-9]+$"
+pLetters = re.compile("^[a-zA-Z]+$")
+pNumbers = re.compile("^[0-9]+$")
+pEmail = re.compile(
+    '^([a-zA-Z0-9]+)(\\.[a-zA-Z0-9]+)?@([a-zA-Z]+)\\.[a-zA-Z]{2,3}$')
+pPasswordCharacterCriteria = re.compile("^[a-zA-Z0-9@#\$%\&]{6}$")
+def passwordLengthCriteria(arg): return len(arg) >= 6
 
 
-def isString(arg):
-    return (type(arg) == str and len(arg) > 0)
+def isString(arg): return (type(arg) == str and len(arg) > 0)
 
 
 def isLetters(arg):
-    return (type(arg) == str and len(arg) > 0 and re.search(pLetters, arg))
+    return (type(arg) == str and len(arg) > 0 and pLetters.search(arg))
 
 
 def isNumbers(arg):
-    return ((type(arg) == int or type(arg) == float) and re.search(pNumbers, arg))
+    return ((type(arg) == int or type(arg) == float) and pNumbers.search(arg))
 
 
-def isEmail(arg):
-    re.compile(
-        '^([a-zA-Z0-9]+)(\\.[a-zA-Z0-9]+)?@([a-zA-Z]+)\\.[a-zA-Z]{2,3}$')
-    return re.search(arg)
+def isEmail(arg): return pEmail.search(arg)
+
+
+def passwordValidLength(arg): return passwordLengthCriteria(arg)
+
+
+def isValidPassword(arg): return pPasswordCharacterCriteria.search(arg)
